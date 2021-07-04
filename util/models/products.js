72627a9fe43004f12b3798028bb4aad9963088d6
Allister <allisterrampenthal@gmail.com>
2021-07-04 13:35:56 -0500

    setting up product models

---
+++ util/models/products.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  uid: { type: String, trim: true },
+  title: { type: String, trim: true },
+  price: Number,
+  quantity: Number,
+  category: { type: String, trim: true, lowercase: true },
+  sub_category: { type: String, trim: true, lowercase: true },
+  color: { type: String, trim: true, lowercase: true },
+  length: Number,
+  width: Number,
+  height: Number,
+  images: Array,
+  date_Updated: { type: Date, default: Date.now },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
