dcda3cb30f3cfabfa3c6595777e7f2ab2601037c
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-06 10:18:39 -0500

    Update products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,17 +1,61 @@
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const { encode, decode } = require("../../util/encoder.js");
+const ProductsDb = require("../../util/models/products.js");
 
 async function verifyAPIKey(givenKey) {
   return decode(givenKey);
 }
 
 router.route("/").get(async (req, res) => {
-  const verified = await verifyAPIKey(req.header("hfb-apikey"));
+  let verified = await verifyAPIKey(req.header("hfb-apikey"));
   if (verified) {
-    res.json({
-      test: "hey its working now",
+    ProductsDb.find().then((foundProducts) => {
+      res.json(foundProducts);
     });
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.route("/create").post(async (req, res) => {
+  let verified = await verifyAPIKey(req.header("hfb-apikey"));
+  if (verified) {
+    ProductsDb.create(req.body).then((newProduct) => {
+      res.json(newProduct);
+    });
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.route("/update/:id").post(async (req, res) => {
+  let verified = await verifyAPIKey(req.header("hfb-apikey"));
+  if (verified) {
+    ProductsDb.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      returnOriginal: false,
+    })
+      .then((updatedProduct) => {
+        res.sendStatus(200);
+      })
+      .catch((err) =>
+        res.status(422).send("******** Error updating product *********")
+      );
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+router.route("/delete/:id").get(async (req, res) => {
+  let verified = await verifyAPIKey(req.header("hfb-apikey"));
+  if (verified) {
+    ProductsDb.deleteOne({ _id: req.params.id })
+      .then((deletedProduct) => {
+        res.sendStatus(200);
+      })
+      .catch((err) =>
+        res.status(422).send("******** Error deleting product *********")
+      );
   } else {
     res.sendStatus(403);
   }
