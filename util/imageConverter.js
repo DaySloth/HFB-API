28cc202b1d164e7358b1e12033e4d9bd390625a3
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-21 16:43:32 -0500

    Create imageConverter.js

---
+++ util/imageConverter.js
@@ -0,0 +1,14 @@
+const multer = require("multer");
+const DatauriParser = require("datauri/parser");
+
+const path = require("path");
+
+const storage = multer.memoryStorage();
+const multerUploads = multer({ storage }).single("image");
+const parser = new DatauriParser();
+
+const dataUri = async (req) =>
+  parser.format(path.extname(req.file.originalname).toString(), req.file.buffer)
+    .content;
+
+module.exports = { multerUploads, dataUri };
