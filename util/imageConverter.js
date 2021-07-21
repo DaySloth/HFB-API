483ed5eb617ea4d7716ff301e2700fa63df5bc30
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-21 18:21:11 -0500

    Update imageConverter.js

--- util/imageConverter.js
+++ util/imageConverter.js
@@ -8,9 +8,7 @@ const multerUploads = multer({ storage }).single("image");
 const parser = new DatauriParser();
 
 const dataUri = async (req) => {
-  console.log("datauri here");
   let fileType = path.extname(req.file.originalname).toString();
-  console.log(typeof fileType);
   return parser.format(fileType, req.file.buffer).content;
 };
 
