83ac4a023011476b90029738550da008892f5e61
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-31 18:45:36 -0500

    Update sender.js

--- util/sender.js
+++ util/sender.js
@@ -5,8 +5,10 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
+const { resetPasswordHTML } = require("./emailHtmlGen.js");
+
 module.exports = {
-  sendEmail: (email, subject, message) => {
+  sendEmail: async (email, subject, message) => {
     const msg = {
       to: `${email}`, // Change to your recipient
       from: {
@@ -14,8 +16,7 @@ module.exports = {
         name: "HFB Mobile Support",
       }, // Change to your verified sender
       subject: `${subject}`,
-      text: "and easy to do anywhere, even with Node.js",
-      html: `<strong>${message}</strong>`,
+      html: await resetPasswordHTML(message),
     };
     sgMail
       .send(msg)
