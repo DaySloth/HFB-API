89d6a0ba382a259a8e04ba835046f3c98eb5e077
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-31 15:25:52 -0500

    Update users.js

--- util/models/users.js
+++ util/models/users.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   first_name: { type: String, trim: true },
   last_name: { type: String, trim: true },
   email: { type: String, trim: true },
+  phone_number: { type: String, trim: true },
   isTempPassword: Boolean,
   hasWebAccess: Boolean,
   password: { type: String, trim: true },
