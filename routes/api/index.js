f3733b475beb224633a0137293b4717b61cf89bc
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-07-30 21:01:59 -0500

    Update index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const productRoutes = require("./products.js");
+const userRoutes = require("./users.js");
 
 // routes
 router.use("/products", productRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
