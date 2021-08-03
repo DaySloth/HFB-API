146a179753b21d2fa8cb2b6c12b87011ddb42486
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-08-03 15:27:37 -0500

    Update users.js

--- util/models/users.js
+++ util/models/users.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  first_name: { type: String, trim: true },
-  last_name: { type: String, trim: true },
-  email: { type: String, trim: true },
-  phone_number: { type: String, trim: true },
-  isTempPassword: Boolean,
-  hasWebAccess: Boolean,
-  password: { type: String, trim: true },
-  date_updated: { type: Date, default: Date.now },
-});
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, trim: true },
+    last_name: { type: String, trim: true },
+    email: { type: String, trim: true },
+    phone_number: { type: String, trim: true },
+    isTempPassword: Boolean,
+    hasWebAccess: Boolean,
+    password: { type: String, trim: true },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
