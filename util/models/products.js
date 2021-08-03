41027807060c125db198f556bae6f5707c0fff7a
Allister Rampenthal <allisterrampenthal@gmail.com>
2021-08-03 15:27:35 -0500

    Update products.js

--- util/models/products.js
+++ util/models/products.js
@@ -1,21 +1,23 @@
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  uid: { type: String, trim: true },
-  title: { type: String, trim: true },
-  price: Number,
-  quantity: Number,
-  category: { type: String, trim: true },
-  part_num: { type: String, trim: true },
-  sub_category: { type: String, trim: true },
-  finish: { type: String, trim: true },
-  length: Number,
-  width: Number,
-  height: Number,
-  image: { type: String, trim: true },
-  image_id: { type: String, trim: true },
-  date_updated: { type: Date, default: Date.now },
-});
+const productSchema = new mongoose.Schema(
+  {
+    uid: { type: String, trim: true },
+    title: { type: String, trim: true },
+    price: Number,
+    quantity: Number,
+    category: { type: String, trim: true },
+    part_num: { type: String, trim: true },
+    sub_category: { type: String, trim: true },
+    finish: { type: String, trim: true },
+    length: Number,
+    width: Number,
+    height: Number,
+    image: { type: String, trim: true },
+    image_id: { type: String, trim: true },
+  },
+  { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 
